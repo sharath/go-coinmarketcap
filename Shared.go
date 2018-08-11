aaf89e882b061130c8ae3c43d6102b927107bf03
sharath <sharathramku@umass.edu>
2018-08-11 13:16:41 -0400

    created shared structs file

---
+++ Shared.go
@@ -0,0 +1,11 @@
+package go_coinmarketcap
+
+import "time"
+
+type CMCStatus struct {
+	Timestamp    time.Time `json:"timestamp"`
+	ErrorCode    int       `json:"error_code"`
+	ErrorMessage string    `json:"error_message"`
+	Elapsed      int       `json:"elapsed"`
+	CreditCount  int       `json:"credit_count"`
+}
