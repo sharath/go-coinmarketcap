81fc20a8f49e28a402468bc8e8155f977e077116
sharath <sharathramku@umass.edu>
2018-08-11 13:16:24 -0400

    added /cryptocurrency/map

--- Cryptocurrency.go
+++ Cryptocurrency.go
@@ -5,18 +5,13 @@ import (
 	"errors"
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 	"time"
 )
 
-type Info map[string]TokenInfo
-
-type CryptocurrencyResponse struct {
-	Data   Info   `json:"data"`
-	Status Status `json:"status"`
-}
-
-type TokenInfo struct {
+// cryptocurrency/info
+type CryptocurrencyInfoData struct {
 	ID       int      `json:"id"`
 	Name     string   `json:"name"`
 	Symbol   string   `json:"symbol"`
@@ -35,44 +30,110 @@ type TokenInfo struct {
 		Twitter      []string `json:"twitter"`
 	} `json:"urls"`
 }
-
-type Status struct {
-	Timestamp    time.Time `json:"timestamp"`
-	ErrorCode    int       `json:"error_code"`
-	ErrorMessage string    `json:"error_message"`
-	Elapsed      int       `json:"elapsed"`
-	CreditCount  int       `json:"credit_count"`
+type CryptocurrencyInfo map[string]CryptocurrencyInfoData
+type CryptocurrencyInfoResponse struct {
+	Data   CryptocurrencyInfo `json:"data"`
+	Status CMCStatus          `json:"status"`
 }
 
 // GetInfoByID is a wrapper function for GetInfo
 // Example Usage: GetInfoByID(1, 2, 3)
-func (c *Client) GetInfoByID(ids ...int) (Info, error) {
+func (c *Client) GetInfoByID(ids ...int) (CryptocurrencyInfo, error) {
 	idsStr := strings.Trim(strings.Replace(fmt.Sprint(ids), " ", ",", -1), "[]")
 	return c.GetInfo("id", idsStr)
 }
 
 // GetInfoByTicker is a wrapper function for GetInfo
 // Example Usage: GetInfoBySymbol("BTC", "ETH", "LTC")
-func (c *Client) GetInfoBySymbol(tickers ...string) (Info, error) {
+func (c *Client) GetInfoBySymbol(tickers ...string) (CryptocurrencyInfo, error) {
 	return c.GetInfo("symbol", strings.Join(tickers, ","))
 }
 
 // GetInfo fetches metadeta for specified ids or symbols
 // Example usages: GetInfo("id", "1,2,3")
 //                 GetInfo("symbols", "BTC,ETH,LTC")
-func (c *Client) GetInfo(key, vals string) (Info, error) {
+func (c *Client) GetInfo(key, vals string) (CryptocurrencyInfo, error) {
 	endpt := "info"
-	params := "?" + key + "=" + strings.Replace(vals, " ", "", -1)
-	req, err := http.NewRequest("GET", apiURL+cryptocurrency+endpt+params, nil)
+	req, err := http.NewRequest("GET", apiURL+cryptocurrency+endpt, nil)
 	if err != nil {
 		return nil, err
 	}
+	req.URL.Query().Add(key, vals)
 	req.Header["X-CMC_PRO_API_KEY"] = []string{c.apiKey}
 
 	resp, err := c.client.Do(req)
 	defer resp.Body.Close()
 
-	response := new(CryptocurrencyResponse)
+	response := new(CryptocurrencyInfoResponse)
+	err = json.NewDecoder(resp.Body).Decode(response)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.Status.ErrorCode >= 400 {
+		return nil, errors.New(response.Status.ErrorMessage)
+	}
+	return response.Data, err
+}
+
+// cryptocurrency/map
+type CryptocurrencyMapData struct {
+	ID                  int       `json:"id"`
+	Name                string    `json:"name"`
+	Symbol              string    `json:"symbol"`
+	Slug                string    `json:"slug"`
+	IsActive            bool      `json:"is_active"`
+	FirstHistoricalData time.Time `json:"first_historical_data"`
+	LastHistoricalData  time.Time `json:"last_historical_data"`
+}
+type CryptocurrencyMap []CryptocurrencyMapData
+type CryptocurrencyMapResponse struct {
+	Data   CryptocurrencyMap `json:"data"`
+	Status CMCStatus         `json:"status"`
+}
+
+// GetIDMapFor fetches CMC ID Maps for specified parameters
+func (c *Client) GetIDMapFor(symbols ...string) (CryptocurrencyMap, error) {
+	endpt := "map"
+	req, err := http.NewRequest("GET", apiURL+cryptocurrency+endpt, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	params := strings.Join(symbols, ",")
+	req.URL.Query().Add("symbol", params)
+	req.Header["X-CMC_PRO_API_KEY"] = []string{c.apiKey}
+
+	resp, err := c.client.Do(req)
+	defer resp.Body.Close()
+
+	response := new(CryptocurrencyMapResponse)
+	err = json.NewDecoder(resp.Body).Decode(response)
+	if err != nil {
+		return nil, err
+	}
+
+	if response.Status.ErrorCode >= 400 {
+		return nil, errors.New(response.Status.ErrorMessage)
+	}
+	return response.Data, err
+}
+
+func (c *Client) GetIDMapWhere(status string, start, limit int) (CryptocurrencyMap, error) {
+	endpt := "map"
+	req, err := http.NewRequest("GET", apiURL+cryptocurrency+endpt, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.URL.Query().Add("listing_status", status)
+	req.URL.Query().Add("start", strconv.Itoa(start))
+	req.URL.Query().Add("limit", strconv.Itoa(limit))
+	req.Header["X-CMC_PRO_API_KEY"] = []string{c.apiKey}
+
+	resp, err := c.client.Do(req)
+	defer resp.Body.Close()
+
+	response := new(CryptocurrencyMapResponse)
 	err = json.NewDecoder(resp.Body).Decode(response)
 	if err != nil {
 		return nil, err
