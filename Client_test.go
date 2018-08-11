4819ff2e03537e5b7c1dc3ef241a8eee3d150e15
sharath <sharathramku@umass.edu>
2018-08-11 13:49:42 -0400

    split up files

--- Client_test.go
+++ Client_test.go
@@ -1,8 +1,6 @@
 package go_coinmarketcap
 
 import (
-	"fmt"
-	"os"
 	"testing"
 )
 
@@ -10,58 +8,5 @@ func TestNewClient(t *testing.T) {
 	c := NewClient("123")
 	if c == nil {
 		t.FailNow()
-	}
-}
-
-func TestClient_GetInfo(t *testing.T) {
-	c := NewClient(os.Getenv("CMC_API_KEY"))
-	if c == nil {
-		fmt.Println("Warning: CMC_API_KEY not set.")
-		t.SkipNow()
-	}
-	{
-		data, err := c.GetInfo("symbol", "ETH,BTC,LTC")
-		if err != nil || data == nil {
-			fmt.Println(data, err)
-			t.FailNow()
-		}
-	}
-	{
-		data, err := c.GetInfo("id", "1,2,3")
-		if err != nil || data == nil {
-			fmt.Println(data, err)
-			t.FailNow()
-		}
-	}
-}
-
-func TestClient_GetInfoByID(t *testing.T) {
-	c := NewClient(os.Getenv("CMC_API_KEY"))
-	if c == nil {
-		fmt.Println("Warning: CMC_API_KEY not set.")
-		t.SkipNow()
-
-	}
-	{
-		data, err := c.GetInfoByID(1, 2, 3)
-		if err != nil || data == nil {
-			fmt.Println(data, err)
-			t.FailNow()
-		}
-	}
-}
-
-func TestClient_GetInfoBySymbol(t *testing.T) {
-	c := NewClient(os.Getenv("CMC_API_KEY"))
-	if c == nil {
-		fmt.Println("Warning: CMC_API_KEY not set.")
-		t.SkipNow()
-	}
-	{
-		data, err := c.GetInfoBySymbol("BTC", "ETH")
-		if err != nil || data == nil {
-			fmt.Println(data, err)
-			t.FailNow()
-		}
 	}
 }
---
+++ Cryptocurrency_test.go
@@ -0,0 +1,68 @@
+package go_coinmarketcap
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestClient_GetInfo(t *testing.T) {
+	c := NewClient(os.Getenv("CMC_API_KEY"))
+	if c == nil {
+		fmt.Println("Warning: CMC_API_KEY not set.")
+		t.SkipNow()
+	}
+	{
+		data, err := c.GetInfo("symbol", "ETH,BTC,LTC")
+		if err != nil || data == nil {
+			fmt.Println(data, err)
+			t.FailNow()
+		}
+	}
+	{
+		data, err := c.GetInfo("id", "1,2,3")
+		if err != nil || data == nil {
+			fmt.Println(data, err)
+			t.FailNow()
+		}
+	}
+}
+
+func TestClient_GetInfoByID(t *testing.T) {
+	c := NewClient(os.Getenv("CMC_API_KEY"))
+	if c == nil {
+		fmt.Println("Warning: CMC_API_KEY not set.")
+		t.SkipNow()
+
+	}
+	{
+		data, err := c.GetInfoByID(1, 2, 3)
+		if err != nil || data == nil {
+			fmt.Println(data, err)
+			t.FailNow()
+		}
+	}
+}
+
+func TestClient_GetInfoBySymbol(t *testing.T) {
+	c := NewClient(os.Getenv("CMC_API_KEY"))
+	if c == nil {
+		fmt.Println("Warning: CMC_API_KEY not set.")
+		t.SkipNow()
+	}
+	{
+		data, err := c.GetInfoBySymbol("BTC", "ETH")
+		if err != nil || data == nil {
+			fmt.Println(data, err)
+			t.FailNow()
+		}
+	}
+}
+
+func TestClient_GetIDMapFor(t *testing.T) {
+
+}
+
+func TestClient_GetIDMapWhere(t *testing.T) {
+
+}
